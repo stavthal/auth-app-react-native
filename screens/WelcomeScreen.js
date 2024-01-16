c24072cc486af2cfe57596e479452211c99e3260
Stavros Thalassinos <stavros8003@gmail.com>
2024-01-16 15:19:01 +0200

    improved welcome screen with get request

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,15 +1,45 @@
-import { StyleSheet, Text, View, P } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import {useAuthContext} from "../hooks/useAuthContext";
 import FlatButton from "../components/ui/FlatButton";
 import {Colors} from "../constants/styles";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function WelcomeScreen() {
+  const { token } = useAuthContext();
+
   const {logout} = useAuthContext();
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true); // Initiate loading
+    axios.get(`https://react-native-course-704e8-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${token}`)
+        .then((response) => {
+          setFetchedMessage(response.data);
+          }
+        )
+        .finally(() => {
+          setLoading(false);
+        })
+        .catch((err) => {
+      Alert.alert("A problem has occurred!", "We could not fetch your data. Please try again later.");
+      console.log(err.message);
+    })
+  }, []);
+
+  if (isLoading) {
+    return <LoadingOverlay message={"Loading..."} />
+  }
 
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+
+      {/* Fetched message */}
+      <Text style={{color: Colors.primary800, fontSize: 20, marginVertical: 10}}>{fetchedMessage}</Text>
 
       <View style={styles.buttons}>
         <FlatButton textStyle={styles.buttonText} style={styles.button} onPress={logout}>Logout</FlatButton>
